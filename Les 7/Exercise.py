700f23655a9f3e1cf59c85ecbf9b594f8cd1b57a
Peter <peter.schenkels@student.hu.nl>
2018-09-27 16:38:05 +0200

    LES 7

---
+++ Les 7/Exercise.py
@@ -0,0 +1,23 @@
+#Defenitions
+
+def BMI (Lengte, gewicht):
+    a = (gewicht * 703/ Lengte**2)
+    if 18.5 > a:
+        print("Je hebt ondergewicht")
+    elif 25.0 < a:
+        print("Je hebt overgweicht")
+    else:
+        print("Je bent gezond")
+
+#Inputs
+
+L = int(input("Wat is je lengte in inches? :"))
+G = int(input("Wat is je gewicht in ponden? :"))
+
+
+#Uitvoeringen
+
+BMI(L,G)
+
+
+
